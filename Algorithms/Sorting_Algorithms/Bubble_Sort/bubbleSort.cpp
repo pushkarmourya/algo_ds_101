fa803052c70e491a470997e6f391589ad011bfc7
Pushkar mourya <54908682+pushkarmourya@users.noreply.github.com>
2020-10-10 10:32:13 +0530

    update bubble_sort.cpp
    swapping only adjacent element and no swapping by the address of the element

--- Algorithms/Sorting_Algorithms/Bubble_Sort/bubbleSort.cpp
+++ Algorithms/Sorting_Algorithms/Bubble_Sort/bubbleSort.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 void bubble_sort(int arr[],int n)
 {
-    for(int i = 0;i<n;i++)
+    for(int i = 0;i<n-1;i++)
     {
-        for(int j =0;j<n;j++)
+        for(int j =0;j<n-i-1;j++)
         {
-            if(arr[i] < arr[j])
+            if(arr[j] > arr[j+1])
             {
-                swap(arr[i],arr[j]);
+                swap(&arr[j],&arr[j+1]);
             }
         }
     }
